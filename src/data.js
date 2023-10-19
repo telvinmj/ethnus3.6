91d59f06de6e23bd5b9ebc0b87982e7c70d6707d
telvinmj <telvinmj@gmail.com>
2023-10-19 10:53:16 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "SoundGear",
+    subtitle: "React, Strapi, Stripe and MySQL",
+    description:
+      "A responsive MultiUser E-Commerce Website built with React linked with Stripe Payment portal providing multiple features",
+    image: "./project-8.gif",
+    link: "#",
+  },
+  {
+    title: "NewsMan",
+    subtitle: "React and API",
+    description:
+      "A NewsApp which gives daily categorised news using API",
+    image: "./project-7.gif",
+    link: "#",
+  },
+  {
+    title: "DailyJournal",
+    subtitle: "Express js and JavaScript",
+    description:
+      "A journal Website to make notes of daily activities.",
+    image: "./project-6.gif",
+    link: "#",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "Express js and JavaScript",
+    description:
+      "A Daily Todo app showing date and remainding tasks to do.",
+    image: "./project-5.gif",
+    link: "#",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Express.js",
+  "Node",
+  "MySQL",
+  "Strapi",
+];
